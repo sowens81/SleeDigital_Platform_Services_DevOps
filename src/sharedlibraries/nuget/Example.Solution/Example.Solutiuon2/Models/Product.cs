65ea0d659a18bd6b828a52cabc5b142f1e9a5e33
Steve Owens <stevejowens81@outlook.com>
2022-09-28 17:48:03 +0100

    Trunk branch setup

---
+++ src/sharedlibraries/nuget/Example.Solution/Example.Solutiuon2/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Example.Solutiuon2.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public Product()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
+
