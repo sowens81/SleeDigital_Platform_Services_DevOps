c65e9fc2a57c3fb54cf139e2964ef1408d680a5f
Steve Owens <stevejowens81@outlook.com>
2022-09-28 22:33:20 +0100

    Update

--- src/sharedlibraries/nuget/Example.Solution/Example.Solution/Models/Product.cs
+++ src/sharedlibraries/nuget/Example.Solution/Example.Solution/Models/Product.cs
@@ -3,9 +3,12 @@ public class Product
 {
     public string Id { get; set;}
 
+    public string Password { get; set; }
+
     public Product()
     {
         Id = Guid.NewGuid().ToString();
+        Password = "ABC123";
     }
 
 }
